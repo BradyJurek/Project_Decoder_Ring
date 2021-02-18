4c55a26ab7d62ead88511207e846105ccbe954ed
Brady Jurek <77610851+BradyJurek@users.noreply.github.com>
2021-02-17 20:20:27 -0700

    updated polybius function

--- src/polybius.js
+++ src/polybius.js
@@ -7,7 +7,68 @@ const polybiusModule = (function () {
   // you can add any code you want within this function scope
 
   function polybius(input, encode = true) {
-    // your solution code here
+    const alphabet = {
+      "a": 11,
+      "b": 21,
+      "c": 31,
+      "d": 41,
+      "e": 51,
+      "f": 12,
+      "g": 22,
+      "h": 32,
+      "(i/j)": 42,
+      "k": 52,
+      "l": 13,
+      "m": 23,
+      "n": 33,
+      "o": 43,
+      "p": 53,
+      "q": 14,
+      "r": 24,
+      "s": 34,
+      "t": 44,
+      "u": 54,
+      "v": 15,
+      "w": 25,
+      "x": 35,
+      "y": 45,
+      "z": 55,
+    }
+    
+    let result = "";
+
+    const lowerInput = input.toLowerCase()
+
+    if (!input) {
+      return false
+    }
+    //ENCODING SECTION
+    if (encode) {
+      for (let i = 0; i < lowerInput.length; i++) {
+        const letter = lowerInput[i];
+
+        if (letter === "i" || letter === "j") {
+          result += 42
+        } else if (letter === ' ') {
+          result += ' '
+        } else {
+          const pairs = Object.entries(alphabet).find(pair => letter === pair[0]);
+          result += pairs[1]
+        }
+      }
+    }
+    //DECODING SECTION
+    if (!encode) {
+      if (input.length % 2 == 0){
+        for (let i = 0; i < input.length; i++) {
+
+        }
+      } else {
+        return false;
+      }
+    }
+
+    return result
   }
 
   return {
--- test/polybius.test.js
+++ test/polybius.test.js
@@ -1,1 +1,11 @@
-// Write your tests here!
+const expect = require("chai").expect;
+const polybius = require("../src/polybius.js");
+
+describe("polybius()", () => {
+  it("should return false if nothing is inputed", () => {
+        const expected = false;
+        const actual = polybius();
+        expect(actual).to.eql(expected);
+  });
+
+});
